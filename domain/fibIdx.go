db4f8ce7726aeda66113dd175c59b13d149b5c4a
unagami <kantaunagami@gmail.com>
2025-05-16 00:17:28 +0900

    :+1:  #1 フィボナッチ数列のアルゴリズムを改善 フィボナッチ数列のアルゴリズムのテストコードを追加

--- domain/fibIdx.go
+++ domain/fibIdx.go
@@ -3,24 +3,11 @@ package domain
 import "math/big"
 
 func CalcFibNum(fibIdx *big.Int) (*big.Int, error) {
-	//n = 0, 1のときのフィボナッチ数列の値を返す
-	if fibIdx == big.NewInt(0) {
-		return big.NewInt(0), nil
-	}
-	if fibIdx == big.NewInt(1) {
-		return big.NewInt(1), nil
-	}
-
-	//n = 2以上のときのフィボナッチ数列の値を返す
-	fib0 := big.NewInt(0)
-	fib1 := big.NewInt(1)
-
+	//[[1, 1], [1, 0]]^fibIdx の行列対角成分がフィボナッチ数列の一般項となる
 	fib_matrix := [2][2]*big.Int{{big.NewInt(1), big.NewInt(1)}, {big.NewInt(1), big.NewInt(0)}}
 	fib_matrix_pow := MatrixPow(fib_matrix, fibIdx)
 
-	fibNum := new(big.Int)
-	fibNum.Add(fibNum, fib0.Mul(fib0, fib_matrix_pow[1][0]))
-	fibNum.Add(fibNum, fib1.Mul(fib1, fib_matrix_pow[1][1]))
+	fibNum := fib_matrix_pow[0][1]
 
 	return fibNum, nil
 }
--- domain/fibIdx_helper.go
+++ domain/fibIdx_helper.go
@@ -1,37 +1,44 @@
 package domain
 
-import "math/big"
+import (
+	"math/big"
+)
 
 // 2X2行列同士の掛け算を定義
-func matrixMul(m1 [2][2]*big.Int, m2 [2][2]*big.Int) [2][2]*big.Int {
-	m00 := new(big.Int)
-	m01 := new(big.Int)
-	m10 := new(big.Int)
-	m11 := new(big.Int)
-
-	m00.Add(m00, m1[0][0].Mul(m1[0][0], m2[0][0]))
-	m00.Add(m00, m1[0][1].Mul(m1[0][1], m2[1][0]))
-	m01.Add(m01, m1[0][0].Mul(m1[0][0], m2[0][1]))
-	m01.Add(m01, m1[0][1].Mul(m1[0][1], m2[1][1]))
-	m10.Add(m10, m1[1][0].Mul(m1[1][0], m2[0][0]))
-	m10.Add(m10, m1[1][1].Mul(m1[1][1], m2[1][0]))
-	m11.Add(m11, m1[1][0].Mul(m1[1][0], m2[0][1]))
-	m11.Add(m11, m1[1][1].Mul(m1[1][1], m2[1][1]))
-
-	return [2][2]*big.Int{{m00, m01}, {m10, m11}}
+func matrixMul(a, b [2][2]*big.Int) [2][2]*big.Int {
+	return [2][2]*big.Int{
+		{
+			new(big.Int).Add(
+				new(big.Int).Mul(a[0][0], b[0][0]),
+				new(big.Int).Mul(a[0][1], b[1][0]),
+			),
+			new(big.Int).Add(
+				new(big.Int).Mul(a[0][0], b[0][1]),
+				new(big.Int).Mul(a[0][1], b[1][1]),
+			),
+		},
+		{
+			new(big.Int).Add(
+				new(big.Int).Mul(a[1][0], b[0][0]),
+				new(big.Int).Mul(a[1][1], b[1][0]),
+			),
+			new(big.Int).Add(
+				new(big.Int).Mul(a[1][0], b[0][1]),
+				new(big.Int).Mul(a[1][1], b[1][1]),
+			),
+		},
+	}
 }
 
-// 2^30乗までの行列の累乗を計算する
 func MatrixPow(m [2][2]*big.Int, n *big.Int) [2][2]*big.Int {
-	pm := m
 	ans := [2][2]*big.Int{{big.NewInt(1), big.NewInt(0)}, {big.NewInt(0), big.NewInt(1)}}
-
-	for i := 0; i < 30; i++ {
+	pm := m
+	// n.BitLen() は n を 2 進数で表したときの長さ
+	for i := 0; i < n.BitLen(); i++ {
 		if n.Bit(i) == 1 {
 			ans = matrixMul(ans, pm)
 		}
 		pm = matrixMul(pm, pm)
 	}
-
 	return ans
 }
