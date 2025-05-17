39f4a731d975ce40045c428f3108a25a721a0d2f
unagami <kantaunagami@gmail.com>
2025-05-17 11:51:21 +0900

    :books: ドキュメント

--- main.go
+++ main.go
@@ -10,18 +10,21 @@ import (
 )
 
 func main() {
+	//ポート番号を取得
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
 
+	//依存性の注入
 	fibIdxUsecase := usecase.NewFibIdxUsecase()
 	fibIdxHandler := handler.NewFibIdxHandler(fibIdxUsecase)
 
+	//サーバーの初期化
 	e := echo.New()
-	e.Debug = true
-
+	//ルーティングの設定
+	//GET /fib?n=10
 	e.GET("/fib", fibIdxHandler.HandlerCalcFibNum)
-
+	//サーバー起動
 	e.Logger.Fatal(e.Start(":" + port))
 }
